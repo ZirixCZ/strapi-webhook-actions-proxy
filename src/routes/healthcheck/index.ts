501a60446751ba65b145b5d1a6fe40530dcc7d0a
Michal Vaniš <zirixcz@gmail.com>
2023-07-31 17:40:27 +0200

    make healthcheck usable from strapi trigger

--- src/routes/healthcheck/index.ts
+++ src/routes/healthcheck/index.ts
@@ -1,6 +1,6 @@
 import { Router, Request, Response } from 'express';
 export const healthCheckRoute = Router();
 
-healthCheckRoute.get('/', async (req: Request, res: Response) => {
+healthCheckRoute.post('/', async (req: Request, res: Response) => {
   res.status(200).send('Healthy');
 });
